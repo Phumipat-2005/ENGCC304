a2328cb725494d73fdef0dd1dd72946980fcce14
Phumipat Wongdaw <Phumipat56110@gmail.com>
2025-10-11 10:50:11 +0700

    lab3.1

---
+++ lab/lab3.1/lab3.1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    float height, base;
+    printf("Enter height: ");
+    scanf("%f", &height);
+    printf("Enter base: ");
+    scanf("%f", &base);
+
+    float area = (height * base) / 2;
+    printf("Triangle area = %.1f\n", area);
+    return 0;
+}
