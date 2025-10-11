940c3564f575ab89fb02286ff9389ad47519dc84
Phumipat Wongdaw <Phumipat56110@gmail.com>
2025-10-11 12:17:47 +0700

    lab5

--- lab/lab5/lab5.cpp
+++ lab/lab5/lab5.cpp
@@ -12,21 +12,21 @@ int main() {
 
     // ตรวจสอบเกรด
     if (Grade >= 80) {
-        printf("Grade = A \n");
+        printf("Grade = A !\n");
     } else if (Grade >= 75) {
-        printf("Grade = B+ \n");
+        printf("Grade = B+ !\n");
     } else if (Grade >= 70) {
-        printf("Grade = B \n");
+        printf("Grade = B !\n");
     } else if (Grade >= 65) {
-        printf("Grade = C+ \n");
+        printf("Grade = C+ !\n");
     } else if (Grade >= 60) {
-        printf("Grade = C \n");
+        printf("Grade = C !\n");
     } else if (Grade >= 55) {
-        printf("Grade = D+ \n");
+        printf("Grade = D+ !\n");
     } else if (Grade >= 50) {
-        printf("Grade = D \n");
+        printf("Grade = D !\n");
     } else if (Grade < 50) {
-        printf("Grade = F \n");
+        printf("Grade = F !\n");
     }
     return 0;
 }
